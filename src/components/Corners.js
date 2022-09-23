423997644fa114b25c5c57277cef0610554c3e5c
danianise <danianisehoey@gmail.com>
2022-09-23 11:24:37 -0400

    Initial set up of party invite

---
+++ src/components/Corners.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Corners() {
+  return (<>
+    <div className='cornerWeb'>
+        <img src='cornerSpiderweb.png'/>
+    </div>
+    <div className='cornerHouse'>
+        <img src='hauntedHouse.png'/>
+    </div>
+    </>
+  )
+}
+
+export default Corners
