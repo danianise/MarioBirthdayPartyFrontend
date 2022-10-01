9c34185706b536a7b88d6a972ff15c40b5af2056
danianise <danianisehoey@gmail.com>
2022-10-01 15:04:12 -0400

    First migration for Django backend

--- backend/birthday_app/models.py
+++ backend/birthday_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Response(models.Model):
+    name = models.CharField(max_length=100)
+    guest_count = models.CharField(max_length=100)
+    message = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.name, self.guest_count
