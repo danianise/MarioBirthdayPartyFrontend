ca0ac436190e1e1fb592470835d4eff446b02ebe
danianise <danianisehoey@gmail.com>
2024-03-31 14:39:36 -0400

    checking on mobile

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -4,7 +4,7 @@ function Header() {
   return (
     <div>
         <h1 id="header">Jeffrey Hoey has LEVELED UP to age 7!</h1>
-        <h4 className="mobile">Join us for a Super 7th Birthday Party</h4>
+        <h4 className="mobile">Join us for a<br/>Super 7th Birthday Party</h4>
     </div>
   )
 }
